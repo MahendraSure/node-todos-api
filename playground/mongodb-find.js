92a3f6a199034fb944702f6c98a1095af12461c7
Mahendra <mahindra.sure@gmail.com>
2017-10-20 11:56:50 +0530

    Added script to delete files from Database

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -4,11 +4,21 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
     if(err) return console.log('Unable to connect to MongoDB Server');
     console.log('Connected to MongoDB server');
     
-    db.collection('Users').find({name:"Mahendra"}).toArray().then((docs)=>{
-        console.log(JSON.stringify(docs, undefined, 2));
-    }, (err) =>{
-        console.log('Unable to fetch data from Users collection');
+//    db.collection('Users').deleteMany({name:'Mahendra'});
+    
+    db.collection('Users').findOneAndDelete({
+        _id: ObjectID("59e980008837111bd4ddb86c")
+    }).then((result) =>{
+        console.log(JSON.stringify(result, undefined, 2));
     })
+    
+    
+//    db.collection('Users').find({name:"Mahendra"}).toArray().then((docs)=>{
+//        console.log(JSON.stringify(docs, undefined, 2));
+//    }, (err) =>{
+//        console.log('Unable to fetch data from Users collection');
+//    })
+//    
     
 //    db.collection('Todos').find({_id:ObjectID("59e988f9f44ae098a0112564")}).toArray().then((docs) =>{
 //        console.log('Todos');
